f18694d3c8c41e33256bcfe9af12fa442437230f
Beharudin Mohammed <89333678+Beharudin@users.noreply.github.com>
2026-04-15 16:05:58 +0300

    Added some features

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
@@ -11,6 +12,20 @@ from routes import auth, projects, tasks, websocket, users
 limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI(title="SaaS Project Management API")
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
