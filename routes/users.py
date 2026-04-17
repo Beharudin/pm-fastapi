f2b61d50e860dea341fbe0e77f108720fd171d2b
Beharudin Mohammed <89333678+Beharudin@users.noreply.github.com>
2026-04-17 11:13:15 +0300

    Completed

--- routes/users.py
+++ routes/users.py
@@ -22,8 +22,7 @@ def get_user(user_id, db: Session = Depends(get_db), user=Depends(get_current_us
     user_data = get_user_by_id(db, user_id)
     if not user_data:
         raise HTTPException(status_code=404, detail="User not found")
-    print(f"Current user: {user.email} (ID: {user.id}, Role: {user.role})")
-    print(f"Owner user: {user_data.email} (ID: {user_data.id}, Role: {user_data.role})")
+    
     if(str(user.id) != str(user_data.id) and user.role not in [RoleEnum.admin, RoleEnum.manager]):
         raise HTTPException(status_code=403, detail="Forbidden: Not authorized for this action")
 
